2d4c5845863d42790481f23d3c34d003603b51f3
Annonnymmousss <jatin02012006@gmail.com>
2026-05-13 03:56:52 +0530

    chore: code review

--- node-graph/libraries/graphic-types/src/graphic.rs
+++ node-graph/libraries/graphic-types/src/graphic.rs
@@ -571,7 +571,7 @@ impl BoundingBox for Graphic {
 			Graphic::Graphic(list) => list.bounding_box(transform, include_stroke),
 			Graphic::Color(list) => list.bounding_box(transform, include_stroke),
 			Graphic::Gradient(list) => list.bounding_box(transform, include_stroke),
-			Graphic::Text(_) => RenderBoundingBox::None,
+			Graphic::Text(_) => RenderBoundingBox::Infinite,
 		}
 	}
 
@@ -583,7 +583,7 @@ impl BoundingBox for Graphic {
 			Graphic::Graphic(graphic) => graphic.thumbnail_bounding_box(transform, include_stroke),
 			Graphic::Color(color) => color.thumbnail_bounding_box(transform, include_stroke),
 			Graphic::Gradient(gradient) => gradient.thumbnail_bounding_box(transform, include_stroke),
-			Graphic::Text(_) => RenderBoundingBox::None,
+			Graphic::Text(_) => RenderBoundingBox::Infinite,
 		}
 	}
 }
